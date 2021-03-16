bff9874321ed327e801439ae7009ff81908db798
Dmitriy Bormotov <squier7@gmail.com>
2021-03-16 09:35:44 +0500

    first commit

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,43 +1,31 @@
 import Page from './page';
 
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
 class LoginPage extends Page {
-    get inputUsername () { return $('#normal_login_email'); }
-    get inputPassword () { return $('#normal_login_password'); }
-    get buttonSubmit () { return $('.login-form-button'); }
-    get errorToast () { return $('.ant-notification-notice-message'); }
-    get loginValidationError () { return $('//div[contains(@class, "ant-form-item-with-help")][.//input[@id="normal_login_email"]]//div[@role="alert"]'); }
+    /**
+     * define selectors using getter methods
+     */
+    get inputUsername () { return $('#username') }
+    get inputPassword () { return $('#password') }
+    get btnSubmit () { return $('button[type="submit"]') }
 
-    open () {
-        return super.open('/user/login');
-    }
-
-    setLogin (email) {
-        this.inputUsername.setValue(email);
-    }
-
-    setPassword (password) {
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    login (username, password) {
+        this.inputUsername.setValue(username);
         this.inputPassword.setValue(password);
+        this.btnSubmit.click(); 
     }
 
-    clickSubmitButton () {
-        this.buttonSubmit.click();
-    }
-
-    submitButtonIsDisabled() {
-        expect(this.buttonSubmit).toBeDisabled();
-    }
-
-    errorToastAppeared() {
-        expect(this.errorToast).toBeDisplayed();
-    }
-
-    emptyLoginInput() {
-        this.clearInput(this.inputUsername);
-    }
-
-    loginRequiredError() {
-        expect(this.loginValidationError).toBeDisplayed();
-        expect(this.loginValidationError.getText()).toEqual('Required');
+    /**
+     * overwrite specifc options to adapt it to page object
+     */
+    open () {
+        return super.open('login');
     }
 }
 
