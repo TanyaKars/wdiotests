70e6edbc64dc67b7a267a239afc162a9c4645415
TanyaKars <euphratus.tk@gmail.com>
2021-03-25 12:52:20 -0700

    2 Commit

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,10 +1,12 @@
-import Page from './page';
+import Page from './page.js';
 
 class LoginPage extends Page {
 
     get inputUsername () { return $('#normal_login_email'); }
     get inputPassword () { return $('#normal_login_password'); }
     get SubmitButton () { return $('.login-form-button'); }
+    get InputWrongPassword () { return $('#normal_login_password');}
+    get ErrorMessage () {return $('.ant-notification-notice-message');}
 
     setLogin (email){
         this.inputUsername.setValue(email);
@@ -14,13 +16,28 @@ class LoginPage extends Page {
         this.inputPassword.setValue(password);
     }
 
+    setWrongPassword (wrongPassword){
+        this.InputWrongPassword.setValue(wrongPassword);
+    }
+
+    setError (error){
+        expect(this.ErrorMessage);
+    }
+
     clickSubmitButton (){
         this.SubmitButton.click();
+    }
+
+    SubmitButtonIsDisabled(){
+        expect(this.SubmitButton).toBeDisabled()
     }
 
     open () {
         return super.open('/');
     }
+
+
+
 }
 
 export default new LoginPage();
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,13 +1,34 @@
-import LoginPage from  '../pageobjects/login.page';
-import ProfilePage from '../pageobjects/profile.page'
+import LoginPage from '../pageobjects/login.page.js';
+import ProfilePage from '../pageobjects/profile.page.js';
+
 
 describe('Auth', () => {
-    it('User login with valid data', () => {
+    beforeEach( () =>{
         LoginPage.open();
+    });
+    afterEach(() => {
+        browser.execute('window.localStorage.clear()')
+    });
+
+    it('User login with valid data', () => {
+
         LoginPage.setLogin('nawopav413@gameqo.com');
         LoginPage.setPassword('qwerty');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
     });
+
+    it('submit button is disabled if login and password are absent',() =>{
+        LoginPage.SubmitButtonIsDisabled();
+    });
+
+    it('Error message appears if password incorrect',() => {
+        LoginPage.setLogin('nawopav413@gameqo.com');
+        LoginPage.setWrongPassword('lakhb');
+        LoginPage.clickSubmitButton();
+        LoginPage.setError();
+
+    });
+
 });
 
